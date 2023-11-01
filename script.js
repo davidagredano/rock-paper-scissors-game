0e66cebb9145de74bb9307a4160145b08eef8af2
David Agredano <david.agredano@gmail.com>
2023-11-01 14:31:11 +0100

    Add getComputerChoice() function
    That function will randomly return either "Rock", "Paper" or "Scissors"
    We'll use this function in the game to make the computer's play

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+const CHOICES = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(choices) {
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+console.log(getComputerChoice(CHOICES));
