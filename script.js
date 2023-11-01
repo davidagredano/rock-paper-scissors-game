9d8ceca575e41d5d417d4e8e12a970f4db9d7758
David Agredano <david.agredano@gmail.com>
2023-11-01 15:14:49 +0100

    Add `playRound` function
    The `playRound` function take two parameters (`playerSelection` and
    `computerSelection`) and return a string that declares the winner of the
    round like so: "You Lose! Paper beats Rock"
    
    The function's playerSelection parameter is case-insensitive
    
    Also add a helper function `capitalize` to format the strings equally

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ function getComputerChoice(choices) {
   return choices[randomIndex];
 }
 
-console.log(getComputerChoice(CHOICES));
+const capitalize = (str) =>
+  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = capitalize(playerSelection);
+  const encounter = playerSelection + " vs " + computerSelection;
+  switch (encounter) {
+    case "Rock vs Scissors":
+    case "Scissors vs Paper":
+    case "Paper vs Rock":
+      return `You Won! ${playerSelection} beats ${computerSelection}`;
+    case "Scissors vs Rock":
+    case "Paper vs Scissors":
+    case "Rock vs Paper":
+      return `You Lose! ${playerSelection} is beatten by ${computerSelection}`;
+    default:
+      return `It's a tie! ${playerSelection} equals ${computerSelection}`;
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice(CHOICES);
+console.log(playRound(playerSelection, computerSelection));
